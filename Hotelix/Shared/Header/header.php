be72c0db39b7e571565ffc169fb2f186cd93e5dd
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2024-12-14 22:42:48 +0600

    resolve

--- Hotelix/Shared/Header/header.php
+++ Hotelix/Shared/Header/header.php
@@ -20,26 +20,22 @@
         <div class="max-w-7xl mx-auto flex justify-between items-center">
             <!-- ====== Logo or Brand Name ======== -->
             <a href="#" class="cursor-pointer">
-<<<<<<< HEAD
                 <img src="<?php echo './hotelix/assets/hotel_logo/hotelix.png'; ?>" alt="Hotelix_logo"
                     class="w-[150px]">
-=======
-                <img src="<?php echo './Hotelix/assets/hotelLogo/hotelix.png'; ?>" alt="Hotelix_logo" class="w-[150px]">
->>>>>>> 130f687fc7c3678607c850fc5da817d24c96c271
             </a>
 
             <!-- ======= Navbar links for larger screens ========= -->
             <div class="hidden md:flex space-x-8">
                 <ul class="flex space-x-8">
                     <li
-                        class="p-2 text-[--serondary-color] hover:bg-emerald-400 border-r-2 border-gray-900 hover:border-white rounded-sm transition-all">
+                        class="p-2 text-[--serondary-color] hover:bg-emerald-400 border-r-2 border-transparent hover:border-[--border-color] rounded-sm transition-all">
                         <a href="#">Home</a>
                     </li>
 
                     <!-- ====== Dropdown Menu ======= -->
                     <li class="relative group">
                         <a
-                            class="p-2 text-[--serondary-color] hover:bg-emerald-400 border-r-2 border-gray-900 hover:border-white rounded-sm transition-all flex items-center">
+                            class="p-2 text-[--serondary-color] hover:bg-emerald-400 border-r-2 border-transparent hover:border-[--border-color] rounded-sm transition-all flex items-center">
                             About
                             <svg class="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg">
@@ -60,37 +56,38 @@
                     </li>
 
                     <li
-                        class="p-2 text-[--serondary-color]e hover:bg-emerald-400 border-r-2 border-gray-900 hover:border-white rounded-sm transition-all">
+                        class="p-2 text-[--serondary-color] hover:bg-emerald-400 border-r-2 border-transparent hover:border-[--border-color] rounded-sm transition-all">
                         <a href="#">Gallery</a>
                     </li>
                     <li
-                        class="p-2 text-[--serondary-color] hover:bg-emerald-400 border-r-2 border-gray-900 hover:border-white rounded-sm transition-all">
+                        class="p-2 text-[--serondary-color] hover:bg-emerald-400 border-r-2 border-transparent hover:border-[--border-color] rounded-sm transition-all">
                         <a href="#">Contact</a>
                     </li>
 
-                    <li id="togglebg"
-                        class="p-2 text-[--serondary-color] hover:bg-emerald-400 border-r-2 border-gray-900 hover:border-white rounded-sm transition-all">
-                        <a href="">toggle</a>
-                    </li>
+
                     <label class="swap swap-rotate">
                         <!-- this hidden checkbox controls the state -->
                         <input type="checkbox" class="icon" value="" onclick="handleToggleBtn()" />
 
                         <!-- sun icon -->
-                        <svg class="swap-off h-10 w-10 fill-current" xmlns="http://www.w3.org/2000/svg"
+                        <svg class="swap-off h-5 w-5 fill-current" xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 24 24">
                             <path
                                 d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
                         </svg>
 
                         <!-- moon icon -->
-                        <svg class="swap-on h-10 w-10 fill-current" xmlns="http://www.w3.org/2000/svg"
+                        <svg class="swap-on h-5 w-5 fill-current" xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 24 24">
                             <path
                                 d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
                         </svg>
                     </label>
                 </ul>
+                <div
+                    class="bg-[--primary-color] border-2 border-blue-400 px-3 flex items-center hover:border-green-600 rounded-sm transition-all">
+                    <a href="#">Book Now</a>
+                </div>
             </div>
 
             <!-- ====== Hamburger Menu for small devices ======== -->
@@ -106,15 +103,17 @@
         </div>
 
         <!-- ======= Mobile menu (hidden by default) ======== -->
-        <div id="mobileMenu" class="md:hidden hidden bg-gray-900">
+        <div id="mobileMenu" class="md:hidden hidden bg-[--primary-color]">
             <ul>
-                <li class="p-2 text-white hover:bg-emerald-400 rounded-sm transition-all">
+                <li
+                    class="p-2 text-[--serondary-color] hover:bg-emerald-400 rounded-sm transition-all border-r-2 border-transparent hover:border-[--border-color]">
                     <a href="#">Home</a>
                 </li>
 
                 <!-- ====== Dropdown Menu ======= -->
                 <li class="relative">
-                    <button class="p-2 text-white w-full flex justify-between items-center">
+                    <button
+                        class="p-2 text-[--serondary-color] w-full flex justify-between items-center border-r-2 border-transparent hover:border-[--border-color]">
                         About
                         <svg class="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg">
@@ -133,15 +132,38 @@
                     </ul>
                 </li>
 
-                <li class="p-2 text-white hover:bg-emerald-400 rounded-sm transition-all">
+                <li
+                    class="p-2 text-[--serondary-color] hover:bg-emerald-400 rounded-sm transition-all border-r-2 border-transparent hover:border-[--border-color]">
                     <a href="#">Gallery</a>
                 </li>
-                <li class="p-2 text-white hover:bg-emerald-400 rounded-sm transition-all">
+                <li
+                    class="p-2 text-[--serondary-color] hover:bg-emerald-400 rounded-sm transition-all border-r-2 border-transparent hover:border-[--border-color]">
                     <a href="#">Contact</a>
                 </li>
 
+                <label class="swap swap-rotate mb-3">
+                    <!-- this hidden checkbox controls the state -->
+                    <input type="checkbox" class="icon" value="" onclick="handleToggleBtn()" />
 
+                    <!-- sun icon -->
+                    <svg class="swap-off h-5 w-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path
+                            d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                    </svg>
+
+                    <!-- moon icon -->
+                    <svg class="swap-on h-5 w-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path
+                            d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                    </svg>
+                </label>
             </ul>
+
+            <div
+                class="bg-[--primary-color] border-2 border-blue-400 px-3 flex items-center justify-center hover:border-green-600 rounded-sm transition-all">
+                <a href="#">Book Now</a>
+            </div>
+
         </div>
     </nav>
 
